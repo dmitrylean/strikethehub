ebd93c2d9d2535cad5ef6ca6a2cb5e31d3ab4179
dmitrylean <adam.lean.92@gmail.com>
2025-10-16 09:37:17 +0800

    migrate on vite

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,21 +3,23 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <section className="hero py-16 bg-black text-white bg-[url('/public/assets/img/hero-image.jpg')] bg-no-repeat bg-center bg-cover h-screen">
+    <section className="hero py-16 bg-black text-white bg-[url('/assets/img/hero-image.jpg')] bg-no-repeat bg-center bg-cover h-screen">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
-
         {/* Left Column */}
         <div className="hero-text max-w-lg mb-10 md:mb-0">
           <span className="subtitle text-orange-400 uppercase font-semibold tracking-wide">
             3D game Dev
           </span>
           <h1 className="text-4xl font-bold mt-4 leading-tight">
-            Work that we<br />produce for our<br />
+            Work that we
+            <br />
+            produce for our
+            <br />
             <span className="text-orange-400">clients</span>
           </h1>
           <p className="mt-6 text-gray-300">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard.
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard.
           </p>
           <Link
             to="/about"
